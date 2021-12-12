1aa066fda3085ac73e5c6d6148faed6b46c88003
Zemola <azeemolatunji@gmail.com>
2021-12-12 22:27:28 +0100

    Add functions to the project

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function ageInDays () {
-  let age = prompt('What year were you born');
+  let age = prompt('What year were you born?');
   let aging = (2021-age)*365;
-  
+  let h2 = document.createElement("h2");
+  let text = document.createTextNode(
+    "You have used" + "" + aging + "years on earth"
+  );
+  console.log(text);
+  // h2.appendChild(textnode);
+  let result = document.getElementsByClassName('.result');
+  result.appendChild(text);
+
 
 }
